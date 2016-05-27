dd0d498fcbd6b11490550c064334a4e11bffb808
Scotty Vernon <scott.vernon@gmail.com>
2016-05-27 22:22:28 +0100

    Stupid stupid stupid.

--- config/env/test.js
+++ config/env/test.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
-    host: process.env.DB_HOST,
-    auth: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS
-    }
+    host: '127.0.0.1:5984'//,
+    // auth: {
+    //   username: process.env.DB_USER,
+    //   password: process.env.DB_PASS
+    // }
   }
 }
